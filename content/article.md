e84978d84823efcbf04e0957b0a7fd0c1150cc71
Tomas Vazquez <vazquezt2018@gmail.com>
2024-07-16 12:19:54 -0300

    add new configs

--- content/article.md
+++
@@ -1,4 +0,0 @@
-Title: Mi primer articulo
-Date: 2024.07.16
-
-Este es mi primer artículo
--- pelicanconf.py
+++ pelicanconf.py
@@ -17,16 +17,13 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
+    ("Github", "https://github.com/vazqueztomas/"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("Github", "https://github.com/vazqueztomas/"),
+    ("LinkedIn", "https://www.linkedin.com/in/vazqueztomas21/"),
 )
 
 DEFAULT_PAGINATION = 10
